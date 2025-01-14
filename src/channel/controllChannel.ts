b60e7d27309f043bb5906585af56998379b144f5
Arun Joseph <arunjoseph3007@gmail.com>
2025-01-14 13:36:03 +0530

    Controll Channel protocol added

---
+++ src/channel/controllChannel.ts
@@ -0,0 +1,70 @@
+import * as net from "net";
+import { EventEmitter } from "events";
+import { marshall, unmarshall } from "../message/parser";
+import { ControllMsg, MsgType } from "../message/types";
+
+// The message itself might contain this delimiter and mess up the message
+// This a loop hole and potential cause of failure in the protocol
+// Ideally this should be escaped using back slash or something
+const DELIMITER = "|__|";
+
+type ControllChannelEvents = {
+  ctrlMsg: [ctrlMsg: ControllMsg];
+};
+
+/**
+ * This class encapsulates our custom protocol fro controll channel
+ * It initiates the TCP connection and stores messages in a buffer.
+ * When a new chunk is recieved it check if there is one or more complete messages in the buffer.
+ * When a complete message is recieved it emits `ctrlMsg` with the message recieved
+ */
+export default class ControllChannel extends EventEmitter<ControllChannelEvents> {
+  buffer: string;
+  socket: net.Socket;
+
+  constructor(socket: net.Socket) {
+    super();
+
+    this.buffer = "";
+    this.socket = socket;
+
+    this.socket.on("data", (ch) => {
+      this.buffer += ch.toString();
+      this.processBuffer();
+    });
+  }
+
+  static createChannel(port: number, host: string) {
+    const socket = net.createConnection(port, host);
+
+    return new ControllChannel(socket);
+  }
+
+  private processBuffer() {
+    const fragments = this.buffer.split(DELIMITER);
+
+    while (fragments.length > 1) {
+      const msgString = fragments.shift()!;
+      const ctrlMsg = unmarshall(Buffer.from(msgString));
+      this.emit("ctrlMsg", ctrlMsg);
+    }
+
+    this.buffer = fragments[0];
+  }
+
+  public sendCtrlMsg(ctrlMsg: ControllMsg) {
+    this.socket.write(marshall(ctrlMsg) + DELIMITER);
+  }
+
+  public sendStartMsg(requestId: string) {
+    this.sendCtrlMsg({ requestId, type: MsgType.Start });
+  }
+
+  public sendEndMsg(requestId: string) {
+    this.sendCtrlMsg({ requestId, type: MsgType.End });
+  }
+
+  public sendDataMsg(requestId: string, data: string) {
+    this.sendCtrlMsg({ requestId, data, type: MsgType.End });
+  }
+}
