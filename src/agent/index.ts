47a39237223a7ce5982e04ccc5adaebb978d7016
Arun Joseph <arunjoseph3007@gmail.com>
2025-01-18 13:05:01 +0530

    Agent cli first draft

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -1,11 +1,31 @@
 import HTTPAgent from "./http.agent";
 import TCPAgent from "./tcp.agent";
+import { Command } from "commander";
 
-const localPort = parseInt(process.argv[2]);
-const localHost = "localhost";
+const program = new Command("tsunnel");
 
-const remotePort = 8002;
-const remoteHost = "localhost";
+program.description("A local tunnel written in Typescript");
 
-// const tcpAgent = new TCPAgent(remotePort, remoteHost, localPort, localHost);
-const httpAgent = new HTTPAgent(remotePort, remoteHost, localPort, localHost);
+program
+  .command("tcp")
+  .description("Forwards TCP traffic of given port")
+  .argument("port", "port to be forwarded")
+  .option("-h, --host <string>", "host to be forwarded", "localhost")
+  .option("--allow <cidr>", "if provided only these addresses will be allowed")
+  .option("--deny <cidr>", "if provided these addresses will be blocked")
+  .action((port, options) => {
+    port = parseInt(port);
+    const tcpAgent = new TCPAgent(8002, "localhost", port, options.host);
+  });
+
+program
+  .command("http")
+  .description("Forwards HTTP traffic of given port")
+  .argument("port", "port to be forwarded")
+  .option("-h, --host <string>", "host to be forwarded", "localhost")
+  .action((port, options) => {
+    port = parseInt(port);
+    const httpAgent = new HTTPAgent(8002, "localhost", port, options.host);
+  });
+
+program.parse();
